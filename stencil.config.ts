dbadb351d7719fc153fb497a948bedcca38e9dca
Sameer <schhatbar@aakashinformatics.com>
2024-05-08 16:15:03 +0530

    Adding custom package

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { ValueAccessorConfig, angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'custom-component',
@@ -19,6 +20,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    angularOutputTarget({
+      componentCorePackage: '@schhatbar/custom-component-v3',
+      outputType: 'component',
+      directivesProxyFile: 'dist/custom-component/components.ts',
+      directivesArrayFile: 'dist/index.ts',
+    }),
   ],
   testing: {
     browserHeadless: "new",
